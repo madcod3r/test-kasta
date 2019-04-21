e1b7f7735add72c56ace2defd5df6d5e12298b0c
Олег Волошин <vololeg93@gmail.com>
2019-04-21 13:04:24 +0300

    store functions in separate file a little bit of refactoring

---
+++ functions.php
@@ -0,0 +1,210 @@
+<?php
+/**
+ * Print Line
+ *
+ * @param $string
+ */
+function printLn($string = '') {
+    echo $string . PHP_EOL;
+}
+
+/**
+ * Print Error
+ *
+ * @param $errorMessage
+ */
+function printError($errorMessage) {
+    printLn("Error: $errorMessage");
+}
+
+/**
+ * Print default error
+ */
+function error() {
+    printError('Error in JSON');
+    die;
+}
+
+/**
+ * Check if category has more then 10 products
+ *
+ * @param $curl
+ * @param $categoryQuery
+ * @param int $limit
+ * @return int
+ */
+function isCountOfProductsMoreThen($curl, $categoryQuery, $limit = 10)
+{
+    // send get request
+    $curl->get("https://modnakasta.ua/api/v2/product-list$categoryQuery");
+
+    $productList = json_decode($curl->response, true);
+
+    if ((int)count($productList['product-ids']) > $limit) {
+        return 1;
+    }
+
+    return 0;
+}
+
+/**
+ * Get assoc array of categories
+ *
+ * @param $curl
+ * @param null $parentId
+ * @return array
+ */
+function getCategories($curl, $parentId = null)
+{
+    $categories = [];
+
+    $getParams = ['v' => 1];
+
+    if ($parentId) {
+        $getParams['parent-uuid'] = $parentId;
+    }
+
+    // send get request
+    $curl->get('https://modnakasta.ua/api/v2/market/menu/', $getParams);
+
+    // if curl throws an error
+    if ($curl->error) {
+        // display error
+        printError("\n\tcode: $curl->error_code\n\tmessage: $curl->error_message");
+        return [];
+    } else {
+        // if all is good
+        $categoriesFromAPI = json_decode($curl->response, true);
+
+        if (is_array($categoriesFromAPI) && count($categoriesFromAPI) > 0) {
+
+            $countOfCategories = count($categoriesFromAPI['nodes']);
+
+            if ($countOfCategories > 0) {
+
+                foreach ($categoriesFromAPI['nodes'] as $i => $categoryFromAPINode) {
+                    // create category array
+                    $category = [
+                        'uuid' => $categoryFromAPINode['uuid'],
+                        'name' => $categoryFromAPINode['name'],
+                        'url' => $categoryFromAPINode['url'],
+                        'children' => []
+                    ];
+
+                    if ($parentId) {
+                        $category['parent'] = $categoriesFromAPI;
+                    }
+
+                    // if a category has children - get them by recursion
+                    if ((bool)$categoryFromAPINode['has-children'] === true) {
+                        $category['children'] = getCategories($curl, $categoryFromAPINode['uuid']);
+                    }
+                    // if a category does not have children - get count of products
+                    else {
+                        $category['productsMoreThenTen'] = isCountOfProductsMoreThen($curl, $categoryFromAPINode['q']);
+                    }
+                    $categories[] = $category;
+                }
+
+            } else {
+                error();
+            }
+        } else {
+            error();
+        }
+    }
+    return $categories;
+}
+
+/**
+ * Print categories
+ *
+ * @param $categories
+ * @param array $parentCategory
+ * @param int $count
+ * @return array
+ */
+function printCategories($categories, $parentCategory = [], &$count = 1)
+{
+    // create breadcrumbs for category
+    $breadcrumbs = [];
+    foreach ($categories as $category) {
+
+        // if a category had children - get child categories
+        if (isset($category['children']) && count($category['children']) > 0) {
+            $breadcrumbs[$category['url']]['category'] = [
+                'name' => $category['name'],
+                'url' => $category['url'],
+                'percent' => null,
+            ];
+            $breadcrumbs[$category['url']]['parent'] = $parentCategory;
+            $breadcrumbs[$category['url']]['child'] =
+                printCategories($category['children'], $category, $count);
+        }
+        // if no more children - print category
+        else {
+            // print categories which are only has less then 10 products
+            if ($category['productsMoreThenTen'] === 0) {
+
+                //echo $count++ . '. ';
+
+                // display parent categories names if exists
+                if (isset($category['parent'])) {
+                    foreach ($category['parent']['breadcrumbs'] as $breadcrumb) {
+                        echo $breadcrumb['name'] . ' > ';
+                    }
+                }
+
+                // display category name
+                echo "{$category['name']} | ";
+                // display url
+                echo "https://modnakasta.ua/market/{$category['url']}";
+                // display end of a line
+                echo PHP_EOL;
+            }
+        }
+    }
+
+    return $breadcrumbs;
+}
+
+/**
+ * Count of categories
+ *
+ * @param $categories
+ * @return int
+ */
+function countOfCategories($categories) {
+    $count = 0;
+    $count += count($categories);
+
+    foreach ($categories as $category) {
+        if ($category['children']) {
+            $count += countOfCategories($category['children']);
+        }
+    }
+
+    return $count;
+}
+
+/**
+ * Count of categories which are contain less then 10 products
+ *
+ * @param $categories
+ * @return int
+ */
+function countOfSmallCategories($categories) {
+    $count = 0;
+    foreach ($categories as $category) {
+
+        if (count($category['children']) > 0) {
+            $count += countOfSmallCategories($category['children']);
+        } else {
+            if ($category['productsMoreThenTen'] === 0) {
+                $count++;
+            }
+        }
+    }
+
+    return $count;
+}
--- test.php
+++ test.php
@@ -4,193 +4,16 @@
 use Curl\Curl;
 
 require 'vendor/autoload.php';
-
-/**
- * Print Line
- *
- * @param $string
- */
-function printLn($string = '') {
-    echo $string . PHP_EOL;
-}
-
-/**
- * Print Error
- *
- * @param $errorMessage
- */
-function printError($errorMessage) {
-    printLn("Error: $errorMessage");
-}
-
-function error() {
-    printError('Error in JSON');
-    die;
-}
-
-function isCountOfProductsMoreThen($curl, $categoryQuery, $limit = 10)
-{
-    // send get request
-    $curl->get("https://modnakasta.ua/api/v2/product-list$categoryQuery");
-
-    $productList = json_decode($curl->response, true);
-
-    if ((int)count($productList['product-ids']) > $limit) {
-        return 1;
-    }
-
-    return 0;
-}
-
-function getCategories($curl, $parentId = null)
-{
-    $categories = [];
-
-    $getParams = ['v' => 1];
-
-    if ($parentId) {
-        $getParams['parent-uuid'] = $parentId;
-    }
-
-    // send get request
-    $curl->get('https://modnakasta.ua/api/v2/market/menu/', $getParams);
-
-    // if curl throws an error
-    if ($curl->error) {
-        // display error
-        printError("\n\tcode: $curl->error_code\n\tmessage: $curl->error_message");
-        return [];
-    } else {
-        // if all is good
-        $categoriesFromAPI = json_decode($curl->response, true);
-
-        if (is_array($categoriesFromAPI) && count($categoriesFromAPI) > 0) {
-
-            $countOfCategories = count($categoriesFromAPI['nodes']);
-
-            if ($countOfCategories > 0) {
-
-                foreach ($categoriesFromAPI['nodes'] as $i => $categoryFromAPINode) {
-                    // create category array
-                    $category = [
-                        'uuid' => $categoryFromAPINode['uuid'],
-                        'name' => $categoryFromAPINode['name'],
-                        'url' => $categoryFromAPINode['url'],
-                        'children' => []
-                    ];
-
-                    if ($parentId) {
-                        $category['parent'] = $categoriesFromAPI;
-                    }
-
-                    // if a category has children - get them by recursion
-                    if ((bool)$categoryFromAPINode['has-children'] === true) {
-                        $category['children'] = getCategories($curl, $categoryFromAPINode['uuid']);
-                    }
-                    // if a category does not have children - get count of products
-                    else {
-                        $category['productsMoreThenTen'] = isCountOfProductsMoreThen($curl, $categoryFromAPINode['q']);
-                    }
-                    $categories[] = $category;
-                }
-
-            } else {
-                error();
-            }
-        } else {
-            error();
-        }
-    }
-    return $categories;
-}
-
-/**
- * Print categories
- *
- * @param $categories
- * @param array $parentCategory
- * @param int $count
- * @return array
- */
-function printCategories($categories, $parentCategory = [], &$count = 1)
-{
-    // create breadcrumbs for category
-    $breadcrumbs = [];
-    foreach ($categories as $category) {
-
-        // if a category had children - get child categories
-        if (isset($category['children']) && count($category['children']) > 0) {
-            $breadcrumbs[$category['url']]['category'] = [
-                'name' => $category['name'],
-                'url' => $category['url'],
-                'percent' => null,
-            ];
-            $breadcrumbs[$category['url']]['parent'] = $parentCategory;
-            $breadcrumbs[$category['url']]['child'] =
-                printCategories($category['children'], $category, $count);
-        }
-        // if no more children - print category
-        else {
-            // print categories which are only has less then 10 products
-            if ($category['productsMoreThenTen'] === 0) {
-
-                //echo $count++ . '. ';
-
-                // display parent categories names if exists
-                if (isset($category['parent'])) {
-                    foreach ($category['parent']['breadcrumbs'] as $breadcrumb) {
-                        echo $breadcrumb['name'] . ' > ';
-                    }
-                }
-
-                // display category name
-                echo "{$category['name']} | ";
-                // display url
-                echo "https://modnakasta.ua/market/{$category['url']}";
-                // display end of a line
-                echo PHP_EOL;
-            }
-        }
-    }
-
-    return $breadcrumbs;
-}
-
-function countOfCategories($categories) {
-    $count = 0;
-    $count += count($categories);
-
-    foreach ($categories as $category) {
-        if ($category['children']) {
-            $count += countOfCategories($category['children']);
-        }
-    }
-
-    return $count;
-}
-
-function countOfSmallCategories($categories) {
-    $count = 0;
-    foreach ($categories as $category) {
-
-        if (count($category['children']) > 0) {
-            $count += countOfSmallCategories($category['children']);
-        } else {
-            if ($category['productsMoreThenTen'] === 0) {
-                $count++;
-            }
-        }
-    }
-
-    return $count;
-}
+require 'functions.php';
 
 
 /**
- * Here starts a test work
+ * Caching mechanism
  */
 $categories = [];
 $categoriesFile = __DIR__ . '/categories.data';
+
+// get categories from cache
 if (file_exists($categoriesFile)) {
     if ($categoriesSerialized = file_get_contents($categoriesFile)) {
         // get categories from cache
@@ -198,6 +21,7 @@ if (file_exists($categoriesFile)) {
     }
 }
 
+// get categories from API if cache not exists
 if (empty($categories)) {
     $curl = new Curl();
     $curl->setOpt(CURLOPT_SSL_VERIFYPEER, false);
@@ -210,9 +34,10 @@ if (empty($categories)) {
 $countAllCategories = countOfCategories($categories);
 $countSmallCategories = countOfSmallCategories($categories);
 
+// print list of "small" categories
 printCategories($categories);
 
-// display percent of categories
+// display percent of "small" categories
 echo number_format(($countAllCategories / $countSmallCategories), 2) . "% ";
-echo "(из " . $countAllCategories . " категорий)";
+echo "(из $countAllCategories категорий)";
 echo PHP_EOL;
